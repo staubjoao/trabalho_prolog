42292b14b9769ed3947611550b5c15da06a469bc
João Staub <joao.staub42@gmail.com>
2023-04-01 12:47:11 -0300

    exclusão de paciente funcionando, por enquanto

--- base_dados.pl
+++ base_dados.pl
@@ -60,7 +60,8 @@ escrever_arquivo_prolog(NomeArquivo, Nome, Idade, Peso, Altura) :-
     write(Stream, Peso),
     write(Stream, ','),
     write(Stream, Altura),
-    write(Stream, ').\n'),
+    write(Stream, ').'),
+    nl(Stream),
     close(Stream).
 
 % Predicado para ler o arquivo de texto e armazenar as informações dos pacientes em uma lista
@@ -82,6 +83,21 @@ buscar_paciente(Nome, [paciente(Nome, Idade, Peso, Altura)|_], Idade, Peso, Altu
 buscar_paciente(Nome, [_|Resto], Idade, Peso, Altura) :-
     buscar_paciente(Nome, Resto, Idade, Peso, Altura).
 
+% Criação do arquivo pacientes, caso ele não exista
+criar_arquivo_pacientes :-
+    exists_file('pacientes.txt'),
+    write('O arquivo pacientes.txt já existe.'), nl.
+    
+criar_arquivo_pacientes :-
+    \+ exists_file('pacientes.txt'),
+    tell('pacientes.txt'),
+    write(':- dynamic paciente/4.'), nl,
+    told.
+
+main :-
+    criar_arquivo_pacientes,
+    menu.
+
 % consulta/alteração/inclusão/exclusão de pacientes
 menu :-
     writeln('1 - Consultar pacientes'),
@@ -93,9 +109,23 @@ menu :-
     read(Opcao),
     opcao_menu(Opcao).
 
-opcao_menu(1):-
-    ler_arquivo(pacientes, Pacientes),
-    writeln(Pacientes).
+opcao_menu(1) :-
+    consult('pacientes.txt'),
+    findall(paciente(Nome, Idade, Peso, Altura), paciente(Nome, Idade, Peso, Altura), Pacientes),
+    (
+        Pacientes = []
+        -> write('Não há pacientes cadastrados.'), nl
+        ;  write('Pacientes cadastrados:'), nl,
+           listar_pacientes(Pacientes)
+    ).
+
+listar_pacientes([]).
+listar_pacientes([paciente(Nome, Idade, Peso, Altura)|Resto]) :-
+    write(Nome), write(', '),
+    write(Idade), write(' anos, '),
+    write(Peso), write(' kg, '),
+    write(Altura), write(' cm'), nl,
+    listar_pacientes(Resto).
 
 opcao_menu(2):-
     write('Nome do novo paciente: '),
@@ -106,64 +136,87 @@ opcao_menu(2):-
     read(Peso),
     write('Altura do novo paciente: '),
     read(Altura),
-    escrever_arquivo_prolog(pacientes, Nome, Idade, Peso, Altura),
+    escrever_arquivo_prolog('pacientes.txt', Nome, Idade, Peso, Altura),
     writeln('Novo paciente adicionado com sucesso!').
 
 opcao_menu(3):-
-    editar_paciente_arquivo(pacientes, Nome, NovaIdade, NovoPeso, NovaAltura),
+    editar_paciente_arquivo(pacientes, Nome, NovaIdade, NovoPeso, NovaAltura).
 
 opcao_menu(4):-
-    apagar_paciente(vitor)
+    write('Digite o nome do paciente: '),
+    read(Nome),
+    excluir_paciente(Nome).
+
+excluir_paciente(Nome) :-
+    apagar_paciente(Nome),
+    write('Paciente removido com sucesso.'), nl.
+
+excluir_paciente(Nome) :-
+    \+ apagar_paciente(Nome),
+    write('Paciente não existe.'), nl.
+
 
 opcao_menu(5):-
      write('Saindo...').
 
+
 apagar_paciente(Nome) :-
-    % Abrir o arquivo original para leitura
-    open('pacientes', read, ArquivoOriginal),
-    % Criar um arquivo temporário para escrita
-    open('pacientes_temp', write, ArquivoTemp),
-    % Ler cada linha do arquivo original e verificar se ela contém o paciente a ser excluído
-    apagar_paciente_aux(Nome, ArquivoOriginal, ArquivoTemp, Encontrado),
-    % Fechar os arquivos original e temporário
-    close(ArquivoOriginal),
-    close(ArquivoTemp),
-    % Se o paciente não foi encontrado, não renomear o arquivo temporário
-    (Encontrado ->
-        % Apagar o arquivo original e renomear o arquivo temporário para o nome do arquivo original
-        delete_file('pacientes'),
-        rename_file('pacientes_temp', 'pacientes')
-    ;
-        % Apagar o arquivo temporário
-        delete_file('pacientes_temp')
-    ).
-
-apagar_paciente_aux(Nome, ArquivoOriginal, ArquivoTemp, Encontrado) :-
-    % Ler a próxima linha do arquivo original
-    read_line_to_codes(ArquivoOriginal, Linha),
-    % Se a linha não for vazia (EOF), processá-la
-    (Linha \= end_of_file ->
-        % Converter a linha em formato de string
-        atom_codes(AtomLinha, Linha),
-        % Transformar a string em termo Prolog
-        term_to_atom(Paciente, AtomLinha),
-        % Verificar se o termo contém o paciente a ser excluído
-        (Paciente = paciente(Nome, _, _, _) ->
-            % Se o termo contém o paciente a ser excluído, marcar como encontrado e não copiar a linha para o arquivo temporário
-            Encontrado = true
-        ;
-            % Se o termo não contém o paciente a ser excluído, copiar a linha para o arquivo temporário
-            write(ArquivoTemp, Linha),
-            write(ArquivoTemp, '\n')
-        ),
-        % Chamar recursivamente a função para processar a próxima linha
-        apagar_paciente_aux(Nome, ArquivoOriginal, ArquivoTemp, Encontrado)
-    ;
-        % Se a linha for vazia (EOF), terminar o processamento
-        Encontrado = false
-    ).
+    % Lê os dados do arquivo
+    retractall(paciente(_,_,_,_)),
+    consult('pacientes.txt'),
+    retract(paciente(Nome, Idade, Peso, Altura)),
+    % Abre o arquivo para escrita
+    tell('pacientes.txt'),
+    write(':- dynamic paciente/4.'), nl,
+    % Escreve todas as cláusulas novamente, exceto a cláusula do paciente excluído
+    (paciente(N, I, P, A), N \= Nome, write('paciente('), write(N), write(','), write(I), write(','), write(P), write(','), write(A), write(').'), nl, fail ; true),
+    told.
 
 
 
+% func greff para apagar
+% apagar_paciente(Nome) :-
+%     % Abrir o arquivo original para leitura
+%     open('pacientes', read, ArquivoOriginal),
+%     % Criar um arquivo temporário para escrita
+%     open('pacientes_temp', write, ArquivoTemp),
+%     % Ler cada linha do arquivo original e verificar se ela contém o paciente a ser excluído
+%     apagar_paciente_aux(Nome, ArquivoOriginal, ArquivoTemp, Encontrado),
+%     % Fechar os arquivos original e temporário
+%     close(ArquivoOriginal),
+%     close(ArquivoTemp),
+%     % Se o paciente não foi encontrado, não renomear o arquivo temporário
+%     (Encontrado ->
+%         % Apagar o arquivo original e renomear o arquivo temporário para o nome do arquivo original
+%         delete_file('pacientes'),
+%         rename_file('pacientes_temp', 'pacientes')
+%     ;
+%         % Apagar o arquivo temporário
+%         delete_file('pacientes_temp')
+%     ).
 
-
+% apagar_paciente_aux(Nome, ArquivoOriginal, ArquivoTemp, Encontrado) :-
+%     % Ler a próxima linha do arquivo original
+%     read_line_to_codes(ArquivoOriginal, Linha),
+%     write(Linha),
+%     % Se a linha não for vazia (EOF), processá-la
+%     (Linha \= end_of_file ->
+%         % Converter a linha em formato de string
+%         atom_codes(AtomLinha, Linha),
+%         % Transformar a string em termo Prolog
+%         term_to_atom(Paciente, AtomLinha),
+%         % Verificar se o termo contém o paciente a ser excluído
+%         (Paciente = paciente(Nome, _, _, _) ->
+%             % Se o termo contém o paciente a ser excluído, marcar como encontrado e não copiar a linha para o arquivo temporário
+%             Encontrado = true
+%         ;
+%             % Se o termo não contém o paciente a ser excluído, copiar a linha para o arquivo temporário
+%             write(ArquivoTemp, Linha),
+%             write(ArquivoTemp, '\n')
+%         ),
+%         % Chamar recursivamente a função para processar a próxima linha
+%         apagar_paciente_aux(Nome, ArquivoOriginal, ArquivoTemp, Encontrado)
+%     ;
+%         % Se a linha for vazia (EOF), terminar o processamento
+%         Encontrado = false
+%     ).
